423c8c951d37d5b42ee7ca806c6627dca44270db
knight-Security <paliwaladitya24@gmail.com>
2026-09-27 00:00:26 +0530

    in

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Bar from './components/Bar'
+import ImagesShowCase from './components/ImagesShowCase'
 const App = () => {
   return (
-    <div className='h-full w-full'>
-     <Bar/>
+    <div className='h-[50vh] w-[50vw]'>
+     <ImagesShowCase/>
     </div>
   )
 }
